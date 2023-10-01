ec4502d962efef63079bb52b7a26e1c8737fe4a8
muhamadawais764 <muhammadawais0806@gmail.com>
2023-10-01 21:41:05 +0200

    request per creare servizi

--- app/Http/Requests/creaServiziRequest.php
+++ app/Http/Requests/creaServiziRequest.php
@@ -11,7 +11,7 @@ class creaServiziRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class creaServiziRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'nome_servizio' => 'required',
+            'tipo' => 'string',
+            'pezzo' => 'number',
+            'attivo' => 'boolean',
         ];
     }
 }
