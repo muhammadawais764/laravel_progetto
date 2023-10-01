502bd73a97aec5aa424aa53bd7f353b589f7ac9a
muhamadawais764 <muhammadawais0806@gmail.com>
2023-10-01 21:42:10 +0200

    request per modificare utente

--- app/Http/Requests/modificaUtenteRequest.php
+++ app/Http/Requests/modificaUtenteRequest.php
@@ -11,7 +11,7 @@ class modificaUtenteRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class modificaUtenteRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'nome' => 'required',
+            'cognome' => 'required',
+            'email' => 'required|email|unique:users',
+            'telefono' => 'number'
         ];
     }
 }
