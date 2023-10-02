ecb5e4f4dc8b9b83363914c58e9e8f37070e0be2
muhamadawais764 <muhammadawais0806@gmail.com>
2023-10-02 10:32:23 +0200

    metodi per i servizi

--- app/Http/Controllers/serviziController.php
+++ app/Http/Controllers/serviziController.php
@@ -11,14 +11,24 @@ use App\Http\Requests\modificaServiziRequest;
 
 class serviziController extends Controller
 {
-    public function creaServizio(creaServiziRequest $request)
+    /**
+     * crea un servizio 
+     * @param CreaServiziRequest 
+     * @return ServiziResource
+     */
+    public function creaServizio(creaServiziRequest $nuovo_servizio)
     {
         //creazione di utente su base di utente
         //e torna su base di infomazione che serve per mostare su piataforma
-        $servizio = ServiziModel::create([]);
+        $servizio = ServiziModel::create([$nuovo_servizio]);
         return new ServiziResource($servizio);
     }
 
+    /**
+     * modifica un servizio 
+     * @param modificaServiziRequest 
+     * @return ServiziResource
+     */
     public function modificaServizio(modificaServiziRequest $request)
     {
         //modificazione di utente
@@ -27,19 +37,29 @@ class serviziController extends Controller
         return new ServiziResource($servizio);
     }
 
-    public function deleteServizio(Request $request)
+    /**
+     * elimina un servizio 
+     * @param ServiziModel 
+     * @return JsonResponse
+     */
+    public function deleteServizio(ServiziModel $servizio)
     {
         //eliminazione di utente
-        ServiziModel::delete($request->id);
+        $servizio->delete();
         return new JsonResponse([
             'utente e stato eliminato'
         ]);
     }
 
-    public function dettaglioUtente(Request $request)
+    /**
+     * dettaglio per un servizio 
+     * @param Request 
+     * @return ServiziResource
+     */
+    public function dettaglioServizio(Request $request)
     {
         //eliminazione di utente
-        $servizio = ServiziModel::where('id',$request->id);
+        $servizio = ServiziModel::where('id', $request->id);
         return new ServiziResource($servizio);
     }
 }
